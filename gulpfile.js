0bb56d3a466aeee3c65adcfa964b6cffb800fd7d
jessie.zhu <zhuyudian@zhuyudiandeMacBook-Pro.local>
2017-06-28 22:36:36 +0800

    add header.jade and add css to assets

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('sass', function () {
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('_site/css'))
         .pipe(browserSync.reload({stream:true}))
-        // .pipe(gulp.dest('assets/css'));
+        .pipe(gulp.dest('assets/css'));
 });
 
 /**
